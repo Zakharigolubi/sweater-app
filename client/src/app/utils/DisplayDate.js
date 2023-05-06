2cdb4a6d8b1c626ddf3641a1c67123341903ffed
Anya Zakharova <Zakharigolubi@yandex.ru>
2023-05-06 20:36:24 +0300

    upgrade displayDate func & add regular expression

--- client/src/app/utils/DisplayDate.js
+++ client/src/app/utils/DisplayDate.js
@@ -8,38 +8,47 @@ let options = {
 export function displayDate(date) {
   const createdAt = new Date(date)
   const dateNow = new Date()
-  const yearDif = dateNow.getFullYear() - createdAt.getFullYear()
-  if (yearDif === 0) {
-    const dayDif = dateNow.getDay() - createdAt.getDay()
-    if (dayDif === 0) {
-      const hoursDif = dateNow.getHours() - createdAt.getHours()
-      if (hoursDif === 0) {
-        const minutesDif = dateNow.getMinutes() - createdAt.getMinutes()
-        if (minutesDif >= 0 && minutesDif < 2) return '1 минуту назад'
-        if (minutesDif >= 2 && minutesDif < 3) return '2 минуту назад'
-        if (minutesDif >= 3 && minutesDif < 4) return '3 минуту назад'
-        if (minutesDif >= 4 && minutesDif < 5) return '4 минуту назад'
-        if (minutesDif >= 5 && minutesDif < 10) return '5 минут назад'
-        if (minutesDif >= 10 && minutesDif < 30) {
-          return '10 минут назад'
-        }
-        return '30 минут назад'
-      }
-      return (
-        'сегодня в ' +
-        `${createdAt.getHours()}:${createdAt
-          .getMinutes()
-          ?.toString()
-          .padStart(2, '0')}`
-      )
+  const timeDif = dateNow.getTime() - createdAt.getTime()
+  const daysDif = Math.round(timeDif / (1000 * 60 * 60 * 24))
+  if (daysDif === 0) {
+    const hoursDif = dateNow.getHours() - createdAt.getHours()
+    if (hoursDif === 0) {
+      const minutesDif = dateNow.getMinutes() - createdAt.getMinutes()
+      if (minutesDif <= 1) return `1 минуту назад`
+      if (minutesDif > 1 && minutesDif <= 4) return `${minutesDif} минуты назад`
+      if (minutesDif > 4 && minutesDif <= 20) return `${minutesDif} минут назад`
+      if (minutesDif === 21) return `21 минуту назад`
+      if (minutesDif > 21 && minutesDif <= 24)
+        return `${minutesDif} минуты назад`
+      if (minutesDif > 24 && minutesDif <= 30)
+        return `${minutesDif} минут назад`
+      if (minutesDif === 31) return `31 минуту назад`
+      if (minutesDif > 31 && minutesDif <= 34)
+        return `${minutesDif} минуты назад`
+      if (minutesDif > 34 && minutesDif <= 40)
+        return `${minutesDif} минут назад`
+      if (minutesDif === 41) return `41 минуту назад`
+      if (minutesDif > 41 && minutesDif <= 44)
+        return `${minutesDif} минуты назад`
+      if (minutesDif > 44 && minutesDif <= 50)
+        return `${minutesDif} минут назад`
+      if (minutesDif === 51) return `51 минуту назад`
+      if (minutesDif > 51 && minutesDif <= 54)
+        return `${minutesDif} минуты назад`
+      if (minutesDif > 54 && minutesDif <= 59)
+        return `${minutesDif} минут назад`
     }
-    return (
-      'вчера в ' +
-      `${createdAt.getHours()}:${createdAt
+    if (hoursDif === 1) return `1 час назад`
+    if (hoursDif <= 24)
+      return `сегодня в ${createdAt.getHours()}:${createdAt
         .getMinutes()
         ?.toString()
         .padStart(2, '0')}`
-    )
+    if (hoursDif > 24 && hoursDif <= 48)
+      return `вчера в ${createdAt.getHours()}:${createdAt
+        .getMinutes()
+        ?.toString()
+        .padStart(2, '0')}`
   }
   return (
     createdAt.toLocaleString('ru', options) +
