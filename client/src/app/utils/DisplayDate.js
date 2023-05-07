ac643f55d468b6ac0a6be42db1e81f81aff5edd5
Anya Zakharova <Zakharigolubi@yandex.ru>
2023-05-07 20:12:43 +0300

    fix bugs

--- client/src/app/utils/DisplayDate.js
+++ client/src/app/utils/DisplayDate.js
@@ -1,66 +1,60 @@
-let options = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  timezone: 'UTC'
-}
-
 export function displayDate(date) {
   const createdAt = new Date(date)
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+    timeZone: 'Europe/Moscow'
+  }
+  const formatter = new Intl.DateTimeFormat('ru', options)
+  const formattedDate = formatter.format(createdAt)
+
   const dateNow = new Date()
   const timeDif = dateNow.getTime() - createdAt.getTime()
   const daysDif = Math.round(timeDif / (1000 * 60 * 60 * 24))
+
   if (daysDif === 0) {
-    const hoursDif = dateNow.getHours() - createdAt.getHours()
-    if (hoursDif === 0) {
-      const minutesDif = dateNow.getMinutes() - createdAt.getMinutes()
-      if (minutesDif <= 1) return `1 минуту назад`
-      if (minutesDif > 1 && minutesDif <= 4) return `${minutesDif} минуты назад`
-      if (minutesDif > 4 && minutesDif <= 20) return `${minutesDif} минут назад`
-      if (minutesDif === 21) return `21 минуту назад`
-      if (minutesDif > 21 && minutesDif <= 24)
+    const minutesDif = Math.round(timeDif / (1000 * 60))
+
+    if (minutesDif < 60) {
+      if (minutesDif === 0) {
+        return 'сейчас'
+      }
+      if (minutesDif % 10 === 1) return `${minutesDif} минуту назад`
+      if (
+        (minutesDif % 10 === 2 && minutesDif !== 12) ||
+        (minutesDif % 10 === 3 && minutesDif !== 13) ||
+        (minutesDif % 10 === 4 && minutesDif !== 14)
+      ) {
         return `${minutesDif} минуты назад`
-      if (minutesDif > 24 && minutesDif <= 30)
-        return `${minutesDif} минут назад`
-      if (minutesDif === 31) return `31 минуту назад`
-      if (minutesDif > 31 && minutesDif <= 34)
-        return `${minutesDif} минуты назад`
-      if (minutesDif > 34 && minutesDif <= 40)
-        return `${minutesDif} минут назад`
-      if (minutesDif === 41) return `41 минуту назад`
-      if (minutesDif > 41 && minutesDif <= 44)
-        return `${minutesDif} минуты назад`
-      if (minutesDif > 44 && minutesDif <= 50)
-        return `${minutesDif} минут назад`
-      if (minutesDif === 51) return `51 минуту назад`
-      if (minutesDif > 51 && minutesDif <= 54)
-        return `${minutesDif} минуты назад`
-      if (minutesDif > 54 && minutesDif <= 59)
-        return `${minutesDif} минут назад`
+      }
+      return `${minutesDif} минут назад`
     }
-    if (hoursDif === 1) return `1 час назад`
-    if (hoursDif <= 24)
-      return `сегодня в ${createdAt.getHours()}:${createdAt
-        .getMinutes()
-        ?.toString()
-        .padStart(2, '0')}`
-    if (hoursDif > 24 && hoursDif <= 48)
-      return `вчера в ${createdAt.getHours()}:${createdAt
-        .getMinutes()
-        ?.toString()
-        .padStart(2, '0')}`
-  }
-  return (
-    createdAt.toLocaleString('ru', options) +
-    ' в ' +
-    `${createdAt.getHours()}:${createdAt
+    return `сегодня в ${createdAt.getHours()}:${createdAt
       .getMinutes()
       ?.toString()
       .padStart(2, '0')}`
-  )
+  } else if (daysDif === 1) {
+    return `вчера в ${createdAt.getHours()}:${createdAt
+      .getMinutes()
+      ?.toString()
+      .padStart(2, '0')}`
+  } else {
+    return formattedDate
+  }
 }
 
 export function displayDateProfile(date) {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour12: false,
+    timeZone: 'Europe/Moscow'
+  }
   const createdAt = new Date(date)
   return createdAt.toLocaleString('ru', options)
 }
